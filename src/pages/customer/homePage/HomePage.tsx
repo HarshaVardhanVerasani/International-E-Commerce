45cfa378cecd932067dbf09f03cc08a672dbed96
PavanKalyan-Goli <pavan.goli@xforgetech.ai>
2025-04-03 16:48:00 +0530

    merged with develop

--- src/pages/customer/homePage/HomePage.tsx
+++ src/pages/customer/homePage/HomePage.tsx
@@ -2,8 +2,8 @@ import { Box } from "@mui/material";
 import { useContext } from "react";
 import { ThemeContext } from "../../../context/ThemeWrapper";
 import Footer from "../footer/Footer";
-import { homePageStyles } from "./homePageStyles";
 import Header from "./Header/Header";
+import { homePageStyles } from "./homePageStyles";
 
 const HomePage = () => {
   const { colors } = useContext(ThemeContext);
