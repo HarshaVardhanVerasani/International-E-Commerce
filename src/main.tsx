e02b0ba04cbc68fac97f90888093164d220055ea
PavanKalyan-Goli <pavan.goli@xforgetech.ai>
2025-04-09 15:34:56 +0530

    setup redux and done cart and favorties functinolities

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import ThemeProviderWrapper from "./context/ThemeWrapper.tsx";
 import "./index.css";
+import { store } from "./redux/Store.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <ThemeProviderWrapper>
-        <App />
+        <Provider store={store}>
+          <App />
+        </Provider>
       </ThemeProviderWrapper>
     </BrowserRouter>
   </StrictMode>
